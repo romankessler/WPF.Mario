96374381e9fff88f4200e144e372818c3c16b815
Roman Kessler <r.kessler@bst.ch>
2018-02-08 14:32:58 +0100

    todos erfasst

--- mario.eval/UserControls/People/PlayerUserControl.cs
+++ mario.eval/UserControls/People/PlayerUserControl.cs
@@ -1,7 +1,5 @@
 ﻿namespace Mario.Eval.UserControls.People
 {
-    using System;
-    using System.ComponentModel;
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Input;
@@ -17,30 +15,6 @@
             IsBlocked = true;
         }
 
-        public static readonly DependencyProperty KeyMoveDownProperty = DependencyProperty.Register(
-            "KeyMoveDown",
-            typeof(Key),
-            typeof(PlayerUserControl),
-            new PropertyMetadata(default(Key)));
-
-        public static readonly DependencyProperty KeyMoveLeftProperty = DependencyProperty.Register(
-            "KeyMoveLeft",
-            typeof(Key),
-            typeof(PlayerUserControl),
-            new PropertyMetadata(default(Key)));
-
-        public static readonly DependencyProperty KeyMoveRightProperty = DependencyProperty.Register(
-            "KeyMoveRight",
-            typeof(Key),
-            typeof(PlayerUserControl),
-            new PropertyMetadata(default(Key)));
-
-        public static readonly DependencyProperty KeyMoveUpProperty = DependencyProperty.Register(
-            "KeyMoveUp",
-            typeof(Key),
-            typeof(PlayerUserControl),
-            new PropertyMetadata(default(Key)));
-
         private bool _isKeyDownDown;
 
         private bool _isKeyLeftDown;
@@ -49,59 +23,18 @@
 
         private bool _isKeyUpDown;
 
-        public Key KeyMoveDown
-        {
-            get
-            {
-                return (Key)GetValue(KeyMoveDownProperty);
-            }
-            set
-            {
-                SetValue(KeyMoveDownProperty, value);
-            }
-        }
+        public Key KeyMoveDown { get; set; }
 
-        public Key KeyMoveLeft
-        {
-            get
-            {
-                return (Key)GetValue(KeyMoveLeftProperty);
-            }
-            set
-            {
-                SetValue(KeyMoveLeftProperty, value);
-            }
-        }
+        public Key KeyMoveLeft { get; set; }
 
-        public Key KeyMoveRight
-        {
-            get
-            {
-                return (Key)GetValue(KeyMoveRightProperty);
-            }
-            set
-            {
-                SetValue(KeyMoveRightProperty, value);
-            }
-        }
+        public Key KeyMoveRight { get; set; }
 
-        public Key KeyMoveUp
-        {
-            get
-            {
-                return (Key)GetValue(KeyMoveUpProperty);
-            }
-            set
-            {
-                SetValue(KeyMoveUpProperty, value);
-            }
-        }
-
+        public Key KeyMoveUp { get; set; }
 
         private void PlayerHurt()
         {
             var parent = (MapUserControl)Parent;
-            var newItem = new AnimationControl("ImgBloodFontain", 5, 7, MapItemPosition);
+            var newItem = new AnimationControl("ImgBloodFontain", 5, 7, MapItemPosition); // TODO (ROK): Einbauen, dass animation dem Spieler folgt
             parent.Children.Add(newItem);
         }
 
@@ -113,75 +46,86 @@
             InitializeLife();
         }
 
-
-
         private void InitializeLife()
         {
+            // TODO (ROK): Einbauen, dass die Leben über einen Service gehandelt, initialisiert werden.
             MapItemStati.Life = 5;
         }
 
         private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
         {
+            /* TODO (ROK): Steuerung des Spielers muss in ein separater Service ausgelagert werden. 
+             * Dieser Service soll als control erstellt werden und fokusiert werden.
+             Dadurch können alle Tasten abgerufen werden und auch Spieler 2 kann sich bewegen. 
+
+             Problem zur Zeit: Spieler 2 kann sich nicht bewegen, da der Fokus auf Spieler 1 liegt und somit nur 
+             die Tasteneingabe von Spieler 1 berücksichtigt werden.*/
             Focusable = true;
             Focus();
         }
 
         private void OnPreviewKeyDown(object sender, KeyEventArgs keyEventArgs)
         {
-            
+            /* TODO (ROK): Dieser Teil soll über ein Service gesetzt werden können 
+             * IsMoving = true
+             * MapItemDirection = MapItemDirectionEnum. Left Right Up Down
+             */
 
             if (keyEventArgs.Key == KeyMoveLeft)
             {
                 IsMoving = true;
-                _isKeyLeftDown = true;
+                _isKeyLeftDown = true; // TODO (ROK): kann über ein dictionary im Steuerungsservice gelöst werden
                 MapItemDirection = MapItemDirectionEnum.Left;
             }
 
             if (keyEventArgs.Key == KeyMoveRight)
             {
                 IsMoving = true;
-                _isKeyRightDown = true;
+                _isKeyRightDown = true;// TODO (ROK): kann über ein dictionary im Steuerungsservice gelöst werden
                 MapItemDirection = MapItemDirectionEnum.Right;
             }
 
             if (keyEventArgs.Key == KeyMoveUp)
             {
                 IsMoving = true;
-                _isKeyUpDown = true;
+                _isKeyUpDown = true;// TODO (ROK): kann über ein dictionary im Steuerungsservice gelöst werden
                 MapItemDirection = MapItemDirectionEnum.Up;
             }
 
             if (keyEventArgs.Key == KeyMoveDown)
             {
                 IsMoving = true;
-                _isKeyDownDown = true;
+                _isKeyDownDown = true;// TODO (ROK): kann über ein dictionary im Steuerungsservice gelöst werden
                 MapItemDirection = MapItemDirectionEnum.Down;
             }
         }
 
         private void OnPreviewKeyUp(object sender, KeyEventArgs keyEventArgs)
         {
+            /* TODO (ROK): Dieser Teil soll über ein Service gesetzt werden können 
+             * IsMoving = false*/
+
             if (keyEventArgs.Key == KeyMoveLeft)
             {
-                _isKeyLeftDown = false;
+                _isKeyLeftDown = false;// TODO (ROK): kann über ein dictionary im Steuerungsservice gelöst werden
             }
 
             if (keyEventArgs.Key == KeyMoveRight)
             {
-                _isKeyRightDown = false;
+                _isKeyRightDown = false;// TODO (ROK): kann über ein dictionary im Steuerungsservice gelöst werden
             }
 
             if (keyEventArgs.Key == KeyMoveUp)
             {
-                _isKeyUpDown = false;
+                _isKeyUpDown = false;// TODO (ROK): kann über ein dictionary im Steuerungsservice gelöst werden
             }
 
             if (keyEventArgs.Key == KeyMoveDown)
             {
-                _isKeyDownDown = false;
+                _isKeyDownDown = false;// TODO (ROK): kann über ein dictionary im Steuerungsservice gelöst werden
             }
 
-            if (!_isKeyUpDown
+            if (!_isKeyUpDown// TODO (ROK): kann über ein dictionary im Steuerungsservice gelöst werden
                 && !_isKeyDownDown
                 && !_isKeyLeftDown
                 && !_isKeyRightDown)
@@ -217,7 +161,6 @@
             {
                 PlayerHurt();
             }
-
 
             if (args.NewValue <= 0)
             {
